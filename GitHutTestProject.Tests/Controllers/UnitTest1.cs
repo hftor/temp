0bbf3867d2e3a26418a33283c211162cd8a557e8
haftor2410 <haftor.g@gmail.com>
2014-05-03 19:47:57 +0000

    First time push

---
+++ GitHutTestProject.Tests/Controllers/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitHutTestProject.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIndexMoreThanTen()
+        {
+            List<Translation> translations = new List<Translation>();
+            for (int i = 0; i < 15; i++)
+            {
+                translations.Add(new Translation{
+                    ID = i,
+                    namespace = "Translation " + i.ToString();,
+                    DateTime = DateTime.Now.AddDays(i),
+                    UserWhoAdded = "dabs"
+                }
+            }
+        }
+    }
+}
---
+++ GitHutTestProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitHutTestProject.MvcApplication" Language="C#" %>
